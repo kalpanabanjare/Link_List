3083e1f7fd086f4ca307033f30541a6d2c1f53f8
kalpanabanjare <kalpanabanjare99@gmail.com>
2022-03-23 15:13:42 +0530

    Added: Add node

--- LinkList/LinkList/Program.cs
+++ LinkList/LinkList/Program.cs
@@ -11,10 +11,14 @@ namespace LinkList
             Node node1 = new Node(21);
             Node node2 = new Node(22);
             Node node3 = new Node(23);
+            Node node4 = new Node(24);
+            Node node5 = new Node(25);
 
             linklist.AddNode(node1);
             linklist.AddNode(node2);
             linklist.AddNode(node3);
+            linklist.AddNode(node4);
+            linklist.AddNode(node5);
 
             linklist.Display();
         }
